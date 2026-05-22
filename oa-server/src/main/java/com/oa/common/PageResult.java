d95cb1093805dc172151ceb07288f4330054c974
梦溪 <1269057348@qq.com>
2026-05-22 14:56:47 +0800

    fix: PageResult 添加无参构造函数，修复 Redis 反序列化失败

--- oa-server/src/main/java/com/oa/common/PageResult.java
+++ oa-server/src/main/java/com/oa/common/PageResult.java
@@ -2,9 +2,11 @@ package com.oa.common;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class PageResult<T> {
     private List<T> records;
